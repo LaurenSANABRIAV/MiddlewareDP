af8c2f270d27243f958ffb9298f28c78cb52fc4e
Lauren Sanabria Vargas <lauren.sanabria@font.co.cr>
2021-06-01 09:59:41 -0600

    first commit

---
+++ MiddlewareDosPinos/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiddlewareDosPinos.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareDosPinos.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+     
+
+        public IActionResult Olvido_Contraseña()
+        {
+            return View();
+        }
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+        public IActionResult Crear_Cuenta()
+        {
+            return View();
+        }
+
+    }
+}
